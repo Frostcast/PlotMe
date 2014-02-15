ce3a2d0897bc57ad7a76fc921ae1e8025e1df981
confuser <jamesmortemore@gmail.com>
2014-02-15 11:06:08 +0000

    Teleport denied player to spawn and don't let a player deny themself.

--- src/com/worldcretornica/plotme/listener/PlotDenyListener.java
+++ src/com/worldcretornica/plotme/listener/PlotDenyListener.java
@@ -13,70 +13,57 @@ import com.worldcretornica.plotme.Plot;
 import com.worldcretornica.plotme.PlotManager;
 import com.worldcretornica.plotme.PlotMe;
 
-public class PlotDenyListener implements Listener
-{
+public class PlotDenyListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGH)
-	public void onPlayerMove(final PlayerMoveEvent event)
-	{
+	public void onPlayerMove(final PlayerMoveEvent event) {
 		Player p = event.getPlayer();
-		
-		if(PlotManager.isPlotWorld(p) && !PlotMe.cPerms(p, "plotme.admin.bypassdeny"))
-		{
+
+		if (PlotManager.isPlotWorld(p) && !PlotMe.cPerms(p, "plotme.admin.bypassdeny")) {
 			Location to = event.getTo();
-			
+
 			String idTo = PlotManager.getPlotId(to);
-			
-			if(!idTo.equalsIgnoreCase(""))
-			{
+
+			if (!idTo.equalsIgnoreCase("")) {
 				Plot plot = PlotManager.getPlotById(p, idTo);
-				
-				if(plot != null && plot.isDenied(p.getName()))
-				{
-					event.setCancelled(true);
+
+				if (plot != null && plot.isDenied(p.getName())) {
+					p.teleport(p.getWorld().getSpawnLocation());
 				}
 			}
 		}
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGH)
-	public void onPlayerTeleport(final PlayerTeleportEvent event)
-	{
+	public void onPlayerTeleport(final PlayerTeleportEvent event) {
 		Player p = event.getPlayer();
-		
-		if(PlotManager.isPlotWorld(p) && !PlotMe.cPerms(p, "plotme.admin.bypassdeny"))
-		{
+
+		if (PlotManager.isPlotWorld(p) && !PlotMe.cPerms(p, "plotme.admin.bypassdeny")) {
 			Location to = event.getTo();
-			
+
 			String idTo = PlotManager.getPlotId(to);
-			
-			if(!idTo.equalsIgnoreCase(""))
-			{
+
+			if (!idTo.equalsIgnoreCase("")) {
 				Plot plot = PlotManager.getPlotById(p, idTo);
-				
-				if(plot != null && plot.isDenied(p.getName()))
-				{
+
+				if (plot != null && plot.isDenied(p.getName())) {
 					event.setTo(PlotManager.getPlotHome(p.getWorld(), plot));
 				}
 			}
 		}
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGH)
-	public void onPlayerJoin(final PlayerJoinEvent event)
-	{
+	public void onPlayerJoin(final PlayerJoinEvent event) {
 		Player p = event.getPlayer();
-		
-		if(PlotManager.isPlotWorld(p) && !PlotMe.cPerms(p, "plotme.admin.bypassdeny"))
-		{
+
+		if (PlotManager.isPlotWorld(p) && !PlotMe.cPerms(p, "plotme.admin.bypassdeny")) {
 			String id = PlotManager.getPlotId(p);
-			
-			if(!id.equalsIgnoreCase(""))
-			{
+
+			if (!id.equalsIgnoreCase("")) {
 				Plot plot = PlotManager.getPlotById(p, id);
-				
-				if(plot != null && plot.isDenied(p.getName()))
-				{
+
+				if (plot != null && plot.isDenied(p.getName())) {
 					p.teleport(PlotManager.getPlotHome(p.getWorld(), plot));
 				}
 			}
